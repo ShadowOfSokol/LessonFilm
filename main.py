fd26d623182fc15d84903211ace2b913dcdc3e12
ShadowOfSokol <sokolenkoegor19702002@gmail.com>
2024-11-23 18:19:09 +0200

    Start write a doc about dataset.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn
+import seaborn as sns
 import ast
 
 # download dataset
@@ -9,7 +9,7 @@ url = "movies_metadata.csv"
 movies_df = pd.read_csv(url)
 
 # output info about dataset
-# movies_df.info()
+movies_df.info()
 # print(movies_df.head())
 
 # print(movies_df.describe())
@@ -24,7 +24,6 @@ def extract_genres(gebre_str):
 # print(movies_df['genres'])
 movies_df['genres'] = movies_df['genres'].apply(extract_genres)
 
-
 # print(movies_df['genres'])
 
 movies_df['budget'] = pd.to_numeric(movies_df['budget'], errors='coerce')
@@ -37,8 +36,36 @@ movies_df.dropna(subset=['budget', 'revenue'], inplace = True)
 #----------------------------------------------------------------
 
 # print(movies_df['release_date'])
-movies_df['release_date'] = pd.to_datetime(movies_df['release_date'], errors='coerce').dt.year
-# print(movies_df['release_date'])
+movies_df['release_year'] = pd.to_datetime(movies_df['release_date'], errors='coerce').dt.year
+# print(movies_df['release_year'])
 
 #----------------------------------------------------------------
 
+genres_exploaded = movies_df[['title', 'release_year', 'budget', 'revenue', 'genres']].explode('genres')
+print(genres_exploaded)
+
+genre_count = genres_exploaded['genres'].value_counts()
+print(genre_count)
+
+# Візуалізація жанрів
+plt.figure(figsize=(10,6))
+sns.barplot(x=genre_count.index, y=genre_count.values)
+plt.title("Кількість фільмів за жанрами")
+plt.xlabel("Жанр")
+plt.ylabel("Кілкість фільмів")
+plt.xticks(rotation=45)
+plt.tight_layout()
+# plt.show()
+
+#----------------------------------------------------------------
+movies_df['popularity'] = pd.to_numeric(movies_df['popularity'], errors='coerce')
+popularity_trend = movies_df.groupby('release_year')['popularity'].mean()
+# Візуалізація трендів популярності
+plt.figure(figsize=(12,6))
+popularity_trend.plot()
+plt.title("Середня популярність фільмів за роками")
+plt.xlabel("Рік")
+plt.ylabel("Популярність")
+# plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
